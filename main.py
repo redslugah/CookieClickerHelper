c6775c955438a189d9cab7e7622f2c03f8ce29fb
redslugah <rafael.douglas.1@gmail.com>
2022-07-20 15:16:38 -0300

    iniciando funções

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ import threading
 import keyboard
 
 
+def fast_click():
+    t = threading.current_thread()
+    pyautogui.PAUSE = 0.01
+    while getattr(t, "do_run", True):
+        def clicky():
+            while True:
+                pyautogui.click()
+                if keyboard.is_pressed('F3'):
+                    break
+        keyboard.add_hotkey('F4', clicky)
+        keyboard.wait()
+
+
 def main_window():
     layout = [
         [Sg.Text("Cookie Clicker Helper")],
         [Sg.Button("Ascend", size=(20, 0), key="ascend"),
-         Sg.Button("Fast clicking", size=(20, 0), key="fast"),
+         Sg.Button("Fast click [OFF]", size=(20, 0), key="fastclick"),
          Sg.Button("Savescum", size=(20, 0), key="savescum")],
         [Sg.Button("Sell click combo", size=(20, 0), key="clickcombo"),
          Sg.Button("Stock market bot", size=(20, 0), key="stockBot"),
@@ -20,7 +33,15 @@ def main_window():
         event, values = window.read()
         if event in (Sg.WIN_CLOSED, "exit"):
             break
-
+        elif event == 'fastclick':
+            if window[event].get_text() == 'Fast click [OFF]':
+                window[event].Update("Fast click [ON]", button_color=('white', 'green'))
+                t = threading.Thread(target=fast_click)
+                t.start()
+            else:
+                t.do_run = False
+                t.join()
+                window[event].Update("Fast click [OFF]", button_color=('white', 'blue'))
 
 
 if __name__ == '__main__':
