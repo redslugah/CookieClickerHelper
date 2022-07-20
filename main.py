61b875348229bc674e2e2b556fb11c968ceeeb41
redslugah <rafael.douglas.1@gmail.com>
2022-07-20 11:30:45 -0300

    interface

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import PySimpleGUI as Sg
+import pyautogui
+import threading
+import keyboard
 
 
-# Press the green button in the gutter to run the script.
+def main_window():
+    layout = [
+        [Sg.Text("Cookie Clicker Helper")],
+        [Sg.Button("Ascend", size=(20, 0), key="ascend"),
+         Sg.Button("Fast clicking", size=(20, 0), key="fast"),
+         Sg.Button("Savescum", size=(20, 0), key="savescum")],
+        [Sg.Button("Sell click combo", size=(20, 0), key="clickcombo"),
+         Sg.Button("Stock market bot", size=(20, 0), key="stockBot"),
+         Sg.Button("EXIT", size=(20, 0), key="exit")],
+        [Sg.Output(size=(75, 15), key='-OUTPUT-')]]
+    window = Sg.Window("Cookie Clicker Helper", layout, element_justification='c')
+
+    while True:
+        event, values = window.read()
+        if event in (Sg.WIN_CLOSED, "exit"):
+            break
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main_window()
